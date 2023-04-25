a4b3d0f9969aeba09e02897ffc73cef2e16313db
sanket <wankhdes29@gmail.com>
2023-04-25 14:53:41 +0530

    feat: patient can see filter appointments

--- js/patientAllAppointment.js
+++ js/patientAllAppointment.js
@@ -19,35 +19,64 @@ navabar.innerHTML = navbar;
 
 let allAppointmentArea = document.getElementById("AllAppointmentArea");
 
+let allAppointmentData;
+
 let getAllAppointment = async () => {
   let url = `http://localhost:8888/allAppointment?key=${uuid}`;
 
   let data = await fetch(url);
 
-  let allAppointmentData = await data.json();
+  allAppointmentData = await data.json();
 
-  displayAllAppointment(allAppointmentData);
+  let upcomingAppointment = filterAppointment(
+    allAppointmentData,
+    "upcommingAppointment"
+  );
+
+  displayAllAppointment(upcomingAppointment);
 };
 
 let displayAllAppointment = (allAppointmentData) => {
+  allAppointmentArea.innerHTML = "";
+
   allAppointmentData.forEach((data) => {
-    let html = `
-    
-    <div class="card" style="width: 18rem;">
+    let html = "";
 
-    <img src="${data.doctor.doctorImg}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Appointment</h5>
-            <p>Appointment Data and Time: ${data.appointmentDateAndTime}</p>
-            <p>Doctor Name: ${data.doctor.name}</p>
-            <p>Doctor Experience: ${data.doctor.experience}</p>
-            <p>Doctor Education: ${data.doctor.education}</p>
-            <p>Doctor Specialty: ${data.doctor.specialty}</p>
-            <p>Doctor Contact: ${data.doctor.mobileNo}</p>
-            <a id="disable" class="btn btn-primary updateAppointment">Update Appointment</a>
+    if (data.filterType == "upcommingAppointment") {
+      html = `
+      <div class="card" style="width: 18rem;">
 
-        </div>
-    </div>`;
+      <img src="${data.doctor.doctorImg}" class="card-img-top" alt="...">
+          <div class="card-body">
+              <h5 class="card-title">Appointment</h5>
+              <p>Appointment Data and Time: ${data.appointmentDateAndTime}</p>
+              <p>Doctor Name: ${data.doctor.name}</p>
+              <p>Doctor Experience: ${data.doctor.experience}</p>
+              <p>Doctor Education: ${data.doctor.education}</p>
+              <p>Doctor Specialty: ${data.doctor.specialty}</p>
+              <p>Doctor Contact: ${data.doctor.mobileNo}</p>
+              <a id="disable" class="btn btn-primary upcommingAppointmentButton">Update Appointment</a>
+
+          </div>
+      </div>`;
+    } else {
+      console.log("*****");
+      html = `
+        <div class="card" style="width: 18rem;">
+
+        <img src="${data.doctor.doctorImg}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Appointment</h5>
+                <p>Appointment Data and Time: ${data.appointmentDateAndTime}</p>
+                <p>Doctor Name: ${data.doctor.name}</p>
+                <p>Doctor Experience: ${data.doctor.experience}</p>
+                <p>Doctor Education: ${data.doctor.education}</p>
+                <p>Doctor Specialty: ${data.doctor.specialty}</p>
+                <p>Doctor Contact: ${data.doctor.mobileNo}</p>
+                <a id="disable" class="btn btn-primary pastAppoinmentButton">Review Appointment</a>
+            </div>
+        </div>`;
+    }
 
     allAppointmentArea.innerHTML += html;
 
@@ -55,17 +84,19 @@ let displayAllAppointment = (allAppointmentData) => {
 
     if (date.getTime() < Date.now()) {
       document.getElementById("disable").disabled = true;
-      console.log(document.getElementById("disable"));
+      // console.log(document.getElementById("disable"));
     } else {
-      console.log("This date is fature");
+      // console.log("This date is fature");
     }
   });
 
-  let updateAppointment = document.getElementsByClassName("updateAppointment");
+  let upcommingAppointmentButton = document.getElementsByClassName(
+    "upcommingAppointmentButton"
+  );
 
-  updateAppointment = [...updateAppointment];
+  upcommingAppointmentButton = [...upcommingAppointmentButton];
 
-  updateAppointment.forEach((eachAppointment, index) => {
+  upcommingAppointmentButton.forEach((eachAppointment, index) => {
     eachAppointment.addEventListener("click", (event) => {
       localStorage.setItem(
         "updateAppointment",
@@ -75,6 +106,60 @@ let displayAllAppointment = (allAppointmentData) => {
       window.location.href = "../updateAppointment.html";
     });
   });
+
+  let pastAppoinmentButton = document.getElementsByClassName(
+    "pastAppoinmentButton"
+  );
+
+  pastAppoinmentButton = [...pastAppoinmentButton];
+
+  pastAppoinmentButton.forEach((eachAppointment, index) => {
+    eachAppointment.addEventListener("click", (event) => {
+      localStorage.setItem(
+        "updateAppointment",
+        JSON.stringify(allAppointmentData[index])
+      );
+
+      window.location.href = "../review.html";
+    });
+  });
 };
 
 getAllAppointment();
+
+// filter based on past appointment and upcomming appointment
+
+let appointmentFilter = document.getElementById("appointmentFilter");
+
+appointmentFilter.addEventListener("change", (event) => {
+  let filterValue = appointmentFilter.value;
+
+  console.log(allAppointmentData);
+  let pastAppointments = filterAppointment(allAppointmentData, filterValue);
+
+  displayAllAppointment(pastAppointments);
+});
+
+let filterAppointment = (data, filterValue) => {
+  let presentDate = new Date(); // get the current date and time
+  presentDate = new Date(presentDate.getTime() + 60 * 60 * 1000); // add 1 hour in milliseconds
+
+  let filterData = [];
+
+  data.forEach((eachAppointment) => {
+    const date = new Date(eachAppointment.appointmentDateAndTime);
+    if (filterValue == "upcommingAppointment") {
+      if (presentDate < date) {
+        filterData.push(eachAppointment);
+        eachAppointment.filterType = filterValue;
+      }
+    } else {
+      if (presentDate > date) {
+        filterData.push(eachAppointment);
+        eachAppointment.filterType = filterValue;
+      }
+    }
+  });
+
+  return filterData;
+};
